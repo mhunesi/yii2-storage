7f467092fdb130fae7daf1bcef01600017766205
Mustafa Hayri ÜNEŞİ <mhunesi@gmail.com>
2023-04-18 16:42:29 +0300

    migrate update

--- src/migrations/m221205_120708_create_table_storage_file.php
+++ src/migrations/m221205_120708_create_table_storage_file.php
@@ -19,15 +19,15 @@ class m221205_120708_create_table_storage_file extends Migration
                 'name_new_compound' => $this->string(),
                 'path_prefix' => $this->string(),
                 'file_path' => $this->string(1000),
-                'mime_type' => $this->string(),
-                'extension' => $this->string(),
+                'mime_type' => $this->string(50),
+                'extension' => $this->string(10),
                 'hash_file' => $this->string(),
                 'hash_name' => $this->string(),
-                'file_size' => $this->integer()->defaultValue('0'),
+                'file_size' => $this->integer()->defaultValue(0),
                 'caption' => $this->string(),
-                'inline_disposition' => $this->boolean()->defaultValue('0'),
+                'inline_disposition' => $this->boolean()->defaultValue(0),
                 'parent_id' => $this->integer()->defaultValue(null),
-                'filter_identifier' => $this->string()->defaultValue(null),
+                'filter_identifier' => $this->string(25)->defaultValue(null),
                 'resolution_width' => $this->integer()->defaultValue(null),
                 'resolution_height' => $this->integer()->defaultValue(null),
                 'created_by' => $this->integer(),
@@ -38,11 +38,11 @@ class m221205_120708_create_table_storage_file extends Migration
             ]
         );
 
-        $this->createIndex('index_id_hash_name_is_deleted', '{{%storage_file}}', ['id', 'hash_name', 'is_deleted']);
-        $this->createIndex('index_name_new_compound', '{{%storage_file}}', ['name_new_compound']);
+        $this->createIndex('index_id_hash_name_mime_type_is_deleted', '{{%storage_file}}', ['id', 'hash_name', 'mime_type', 'is_deleted']);
         $this->createIndex('storage_file_index1', '{{%storage_file}}', ['folder_id', 'is_hidden', 'is_deleted', 'name_original']);
-        $this->createIndex('storage_file_index2', '{{%storage_file}}', ['is_deleted', 'id']);
-        $this->createIndex('index_created_user_id', '{{%storage_file}}', ['created_by']);
+		$this->createIndex('storage_file_index2', '{{%storage_file}}', ['is_deleted', 'id']);
+		$this->createIndex('storage_file_index3', '{{%storage_file}}', ['filter_identifier','parent_id','is_deleted']);
+		$this->createIndex('index_created_user_id', '{{%storage_file}}', ['created_by']);
         $this->createIndex('index_updated_user_id', '{{%storage_file}}', ['updated_by']);
     }
 
