88f2b93d81e6f9ade690ce22c3a90691004128d5
Mustafa Hayri ÜNEŞİ <mhunesi@gmail.com>
2023-08-01 23:19:31 +0300

    delete with thumbnails.

--- src/models/StorageFile.php
+++ src/models/StorageFile.php
@@ -50,158 +50,165 @@ use mhunesi\storage\models\StorageImage;
  */
 class StorageFile extends \yii\db\ActiveRecord
 {
-    /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            [
-                'class' => TimestampBehavior::class,
-                'createdAtAttribute' => 'created_at',
-                'updatedAtAttribute' => 'updated_at',
-            ],
-            [
-                'class' => BlameableBehavior::class,
-            ]
-        ];
-    }
-    /**
-     * {@inheritdoc}
-     */
-    public static function tableName()
-    {
-        return 'storage_file';
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function behaviors()
+	{
+		return [
+			[
+				'class' => TimestampBehavior::class,
+				'createdAtAttribute' => 'created_at',
+				'updatedAtAttribute' => 'updated_at',
+			],
+			[
+				'class' => BlameableBehavior::class,
+			]
+		];
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['created_at','updated_at', 'created_by','updated_by', 'resolution_height', 'resolution_width', 'inline_disposition','parent_id'], 'integer'],
-            [['name_original', 'name_new', 'name_new_compound', 'mime_type', 'extension', 'hash_file', 'hash_name','path_prefix','caption'], 'string', 'max' => 255],
-            [['filter_identifier'], 'string', 'max' => 40],
-            [['file_path'], 'string', 'max' => 1000],
-            [['storage_key'], 'string', 'max' => 20],
-            [['name_original', 'name_new', 'mime_type', 'name_new_compound', 'extension', 'hash_file', 'hash_name','storage_key'], 'required'],
-            [['folder_id', 'file_size', 'is_deleted'], 'safe'],
-            [['is_hidden','is_deleted','is_visibility'], 'boolean'],
-            [['path_prefix','file_path','folder_id'], 'trim'],
-            [['path_prefix','file_path','folder_id'], 'default','value' => null],
-            [['is_hidden','is_deleted','is_visibility'], 'default','value' => 0],
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName()
+	{
+		return 'storage_file';
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => Yii::t('storage_file', 'ID'),
-            'storage_key' => Yii::t('storage_file', 'ID'),
-            'is_hidden' => Yii::t('storage_file', 'Is Hidden'),
-            'is_visibility' => Yii::t('storage_file', 'Is Visibility'),
-            'folder_id' => Yii::t('storage_file', 'Folder ID'),
-            'name_original' => Yii::t('storage_file', 'Name Original'),
-            'name_new' => Yii::t('storage_file', 'Name New'),
-            'name_new_compound' => Yii::t('storage_file', 'Name New Compound'),
-            'mime_type' => Yii::t('storage_file', 'Mime Type'),
-            'extension' => Yii::t('storage_file', 'Extension'),
-            'hash_file' => Yii::t('storage_file', 'Hash File'),
-            'hash_name' => Yii::t('storage_file', 'Hash Name'),
-            'file_size' => Yii::t('storage_file', 'File Size'),
-            'is_deleted' => Yii::t('storage_file', 'Is Deleted'),
-            'parent_id' => Yii::t('storage_file', 'Parent ID'),
-            'filter_identifier' => Yii::t('storage_file', 'Filter Identifier'),
-            'resolution_width' => Yii::t('storage_file', 'Resolution Width'),
-            'resolution_height' => Yii::t('storage_file', 'Resolution Height'),
-            'caption' => Yii::t('storage_file', 'Caption'),
-            'path_prefix' => Yii::t('storage_file', 'Path Prefix'),
-            'file_path' => Yii::t('storage_file', 'File Path'),
-            'inline_disposition' => Yii::t('storage_file', 'Inline Disposition'),
-            'created_at' => Yii::t('storage_file', 'Created At'),
-            'updated_at' => Yii::t('storage_file', 'Updated At'),
-            'created_by' => Yii::t('storage_file', 'Created By'),
-            'updated_by' => Yii::t('storage_file', 'Updated By'),
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules()
+	{
+		return [
+			[['created_at', 'updated_at', 'created_by', 'updated_by', 'resolution_height', 'resolution_width', 'inline_disposition', 'parent_id'], 'integer'],
+			[['name_original', 'name_new', 'name_new_compound', 'mime_type', 'extension', 'hash_file', 'hash_name', 'path_prefix', 'caption'], 'string', 'max' => 255],
+			[['filter_identifier'], 'string', 'max' => 40],
+			[['file_path'], 'string', 'max' => 1000],
+			[['storage_key'], 'string', 'max' => 20],
+			[['name_original', 'name_new', 'mime_type', 'name_new_compound', 'extension', 'hash_file', 'hash_name', 'storage_key'], 'required'],
+			[['folder_id', 'file_size', 'is_deleted'], 'safe'],
+			[['is_hidden', 'is_deleted', 'is_visibility'], 'boolean'],
+			[['path_prefix', 'file_path', 'folder_id'], 'trim'],
+			[['path_prefix', 'file_path', 'folder_id'], 'default', 'value' => null],
+			[['is_hidden', 'is_deleted', 'is_visibility'], 'default', 'value' => 0],
+		];
+	}
 
-    /**
-     * Delete a given file.
-     *
-     * Override default implementation. Mark as deleted and remove files from file system.
-     *
-     * Keep file in order to provide all file references.
-     *
-     * @return boolean
-     */
-    public function delete()
-    {
-        if ($this->beforeDelete()) {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id' => Yii::t('storage_file', 'ID'),
+			'storage_key' => Yii::t('storage_file', 'ID'),
+			'is_hidden' => Yii::t('storage_file', 'Is Hidden'),
+			'is_visibility' => Yii::t('storage_file', 'Is Visibility'),
+			'folder_id' => Yii::t('storage_file', 'Folder ID'),
+			'name_original' => Yii::t('storage_file', 'Name Original'),
+			'name_new' => Yii::t('storage_file', 'Name New'),
+			'name_new_compound' => Yii::t('storage_file', 'Name New Compound'),
+			'mime_type' => Yii::t('storage_file', 'Mime Type'),
+			'extension' => Yii::t('storage_file', 'Extension'),
+			'hash_file' => Yii::t('storage_file', 'Hash File'),
+			'hash_name' => Yii::t('storage_file', 'Hash Name'),
+			'file_size' => Yii::t('storage_file', 'File Size'),
+			'is_deleted' => Yii::t('storage_file', 'Is Deleted'),
+			'parent_id' => Yii::t('storage_file', 'Parent ID'),
+			'filter_identifier' => Yii::t('storage_file', 'Filter Identifier'),
+			'resolution_width' => Yii::t('storage_file', 'Resolution Width'),
+			'resolution_height' => Yii::t('storage_file', 'Resolution Height'),
+			'caption' => Yii::t('storage_file', 'Caption'),
+			'path_prefix' => Yii::t('storage_file', 'Path Prefix'),
+			'file_path' => Yii::t('storage_file', 'File Path'),
+			'inline_disposition' => Yii::t('storage_file', 'Inline Disposition'),
+			'created_at' => Yii::t('storage_file', 'Created At'),
+			'updated_at' => Yii::t('storage_file', 'Updated At'),
+			'created_by' => Yii::t('storage_file', 'Created By'),
+			'updated_by' => Yii::t('storage_file', 'Updated By'),
+		];
+	}
 
-            if ($this->storage->fileSystem->delete($this->path)) {
-                Yii::error("Unable to remove file from filesystem: " . $this->path);
-            }
+	/**
+	 * Delete a given file.
+	 *
+	 * Override default implementation. Mark as deleted and remove files from file system.
+	 *
+	 * Keep file in order to provide all file references.
+	 *
+	 * @return boolean
+	 */
+	public function delete()
+	{
+		if ($this->beforeDelete()) {
 
-            $this->updateAttributes(['is_deleted' => true]);
+			if (!$this->storage->fileSystem->delete($this->path)) {
+				Yii::error("Unable to remove file from filesystem: " . $this->path);
+			} else {
+				if (!$this->parent_id) {
+					foreach ($this->children as $child) {
+						$child->delete();
+					}
+				}
 
-            $this->afterDelete();
-            return true;
-        }
+				$this->updateAttributes(['is_deleted' => true]);
+			}
 
-        return false;
-    }
+			$this->afterDelete();
+			return true;
+		}
 
-    public function getChildren()
-    {
-        return $this->hasMany(self::class,['id' => 'parent']);
-    }
+		return false;
+	}
 
-    public function getStorage()
-    {
-        return Yii::$app->storage->getStorage($this->storage_key);
-    }
+	public function getChildren()
+	{
+		return $this->hasMany(self::class, ['parent_id' => 'id']);
+	}
 
-    public function getIsImage()
-    {
-        return in_array($this->mime_type, Yii::$app->storage->imageMimeTypes);
-    }
+	public function getStorage()
+	{
+		return Yii::$app->storage->getStorage($this->storage_key);
+	}
 
-    public function getSizeReadable()
-    {
-        return FileHelper::humanReadableFilesize($this->file_size);
-    }
+	public function getIsImage()
+	{
+		return in_array($this->mime_type, Yii::$app->storage->imageMimeTypes);
+	}
 
-    public function getStream()
-    {
-        return  $this->storage->fileSystem->readStream($this->name_new_compound);
-    }
+	public function getSizeReadable()
+	{
+		return FileHelper::humanReadableFilesize($this->file_size);
+	}
 
-    public function getFileExists()
-    {
-        return $this->storage->fileSystem->has($this->name_new_compound);
-    }
+	public function getStream()
+	{
+		return $this->storage->fileSystem->readStream($this->name_new_compound);
+	}
 
-    public function applyFilter($filterId)
-    {
-        return $this->storage->applyFilter($this->id,$filterId);
-    }
+	public function getFileExists()
+	{
+		return $this->storage->fileSystem->has($this->name_new_compound);
+	}
 
-    public function getPath()
-    {
-        return is_null($this->file_path) ? $this->name_new_compound : ($this->file_path . DIRECTORY_SEPARATOR . $this->name_new_compound);
-    }
+	public function applyFilter($filterId)
+	{
+		return $this->storage->applyFilter($this->id, $filterId);
+	}
 
-    public function getFullPath()
-    {
-        return is_null($this->path_prefix) ? $this->getPath() : ($this->path_prefix . DIRECTORY_SEPARATOR . $this->getPath());
-    }
+	public function getPath()
+	{
+		return is_null($this->file_path) ? $this->name_new_compound : ($this->file_path . DIRECTORY_SEPARATOR . $this->name_new_compound);
+	}
 
-    public function getUrl()
-    {
-        return $this->storage->fileSystem->getUrl($this->path);
-    }
+	public function getFullPath()
+	{
+		return is_null($this->path_prefix) ? $this->getPath() : ($this->path_prefix . DIRECTORY_SEPARATOR . $this->getPath());
+	}
+
+	public function getUrl()
+	{
+		return $this->storage->fileSystem->getUrl($this->path);
+	}
 }
